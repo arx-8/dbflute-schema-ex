4181da2c3667674c284cbbb9507b6e75cc36db52
arx-8 <swans.randomness@gmail.com>
2019-03-03 17:09:39 +0900

    Install eslint-config-airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   "extends": [
-    "eslint:recommended"
+    "airbnb"
   ],
   "plugins": [
     "@typescript-eslint"
@@ -15,5 +15,33 @@ module.exports = {
     "es6": true,
   },
   "rules": {
+    "arrow-body-style": "off",
+    "import/first": "off",
+    "import/no-default-export": "error",
+    "import/prefer-default-export": "off",
+    "no-restricted-properties": [
+      "error",
+      {
+        "property": "innerText",
+        "message": "Use 'textContent' instead. Because 'innerText' is (almost) not recommended."
+      }
+    ],
+    "no-underscore-dangle": [
+      "error",
+      {
+        "allowAfterSuper": true,
+        "allowAfterThis": true
+      }
+    ],
+    "prefer-arrow-callback": [
+      "error",
+      {
+        "allowNamedFunctions": false,
+        "allowUnboundThis": false
+      }
+    ],
+
+    // ts と eslint の相性不良？
+    "import/no-unresolved": "off",
   }
 }
