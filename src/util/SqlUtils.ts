44f443386d681c43e39043d604b004ecd1589fcd
Shin <Sin5hayaK@gmail.com>
2019-03-13 09:31:44 +0900

    AS(エイリアス)の追加 ごり押しver

--- src/util/SqlUtils.ts
+++ src/util/SqlUtils.ts
@@ -5,17 +5,30 @@ export const convertToSql = (
   let result = ""
   const indent = "  "
   let s = 0
+  let AS_name = ""
   result += "SELECT"
   for (let index = 0; index < columnNames.length; index++) {
     let cName = columnNames[s]
-    if (index === columnNames.length - 1) {
-      result += "\n" + indent + cName
-    } else {
-      result += "\n" + indent + cName + ","
+    if (cName.indexOf("member") !== -1) {
+      AS_name = "m"
+      if (index === columnNames.length - 1) {
+        result += "\n" + indent + AS_name + "." + cName
+      } else {
+        result += "\n" + indent + AS_name + "." + cName + ","
+      }
+      s += 1
+    } else if (cName.indexOf("pppp") !== -1) {
+      AS_name = "atn"
+      if (index === columnNames.length - 1) {
+        result += "\n" + indent + AS_name + "." + cName
+      } else {
+        result += "\n" + indent + AS_name + "." + cName + ","
+      }
+      s += 1
     }
-    s += 1
   }
-  result += "\n" + "FROM" + "\n" + indent + tableName + "\n;"
+
+  result += "\n" + "FROM" + "\n" + indent + tableName + " AS " + AS_name + "\n;"
 
   return result
 }
