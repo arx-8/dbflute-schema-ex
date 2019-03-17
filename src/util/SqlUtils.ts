e68e4b9eb5a2389bae6dafdfbae3e0d217684e86
arx-8 <swans.randomness@gmail.com>
2019-03-17 22:52:52 +0900

    feature/converterブランチのためにrevert

--- src/util/SqlUtils.ts
+++ src/util/SqlUtils.ts
@@ -1,6 +1,6 @@
-export const convertToSql = (
+const convertToSql = (
   tableName: string,
   columnNames: ReadonlyArray<string>,
 ): string => {
-  return `${tableName}:${columnNames.join(",")}`
+  return ""
 }
