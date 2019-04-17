8170bfc8599721391c1c4d4b3469d1a473b25e22
arx-8 <swans.randomness@gmail.com>
2019-04-17 23:47:40 +0900

    fix: Show explicit info messages

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,17 @@ import { debugLog } from "./util/Logger"
 const main = (): void => {
   if (!isValidAppTarget()) {
     // dbflute schema html以外で実行された場合は、何もせず終了
-    debugLog("NOP. Cause: No schema html or Invalid.")
+    alert(`No operation.
+
+[Reason] No schema html or Invalid.`)
     return
   }
 
   if (extractSystemStatus() === "INJECTED") {
     // 既に実行済みの場合、何もせず終了
-    debugLog("NOP. Cause: Already injected.")
+    alert(`No operation.
+
+[Reason] Already executed.`)
     return
   }
 
